eaabf8dd403a30376aafbd843af28139b745af59
usman <usmangta@gmail.com>
2020-02-07 04:57:33 +0000

    prints numbers 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+	{
+	int l;
+
+	for (l = 0; l < 10; l++)
+
+	{
+		printf("%d", l);
+	}
+	printf("\n");
+	return (0);
+}
