bb59b01f6d689e8f95c9f2a7284e71bfef68e3b0
usman <usmangta@gmail.com>
2020-02-07 05:29:18 +0000

    prints hexa chars 0-9 a-f using the ascii table

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+        {
+        int l;
+        int u;
+
+        for (l = 48; l <= 57; l++)
+        {
+                putchar(l);
+        }
+        for (u = 97; u <= 102; u++)
+        {
+                putchar(u);
+        }
+        putchar ('\n');
+        return (0);
+}
+
+
