f9ff4370d272eda2b02671616d37c1eef085d130
UsmanGTA <usmangta@gmail.com>
2020-02-09 00:03:45 +0000

    did it!

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+#define ZERO 48
+#define NINE 57
+
+int main (void)
+{
+ int f=0, m=0, l=0;
+
+	for (f = ZERO; f <= NINE; f++)
+	{
+		for (m = ZERO; m <= NINE; m++)
+		{
+			for (l = ZERO; l <= NINE; l++)
+			{
+				if ((f < m) && (m < l))
+				{
+				putchar(f);
+				putchar(m);
+				putchar(l);
+
+					if(f != 55 || m != 56 || l != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+                            	}
+			}
+		}
+	}
+	return (0);
+}
