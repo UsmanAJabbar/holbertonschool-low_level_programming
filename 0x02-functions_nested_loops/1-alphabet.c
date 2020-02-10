e77afb6a6ebbf0b3100d61623359d050fb2c82a1
UsmanGTA <usmangta@gmail.com>
2020-02-10 22:27:32 +0000

    linking all files and outputting all of them

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	int alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+	_putchar(alpha);
+	}
+_putchar('\n');
+}
