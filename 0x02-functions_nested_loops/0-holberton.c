08de96b6bde75d6907dc8dd49c651d6b9a9c380c
UsmanGTA <usmangta@gmail.com>
2020-02-11 15:32:15 +0000

    fixed betty

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,15 +6,12 @@
  * Return: Always 0.
  */
 
-#define ZERO 48
-#define NINE 57
-
 int main(void)
 {
 	char c[] = "Holberton\n";
 	int s;
 
-	for (s = 0; s <= sizeof(c); s++)
+	for (s = 0; s < 11; s++)
 	{
 	_putchar(c[s]);
 	}
