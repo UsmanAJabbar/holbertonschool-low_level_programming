9ea336e87bad976d6f85e600ce413300ed7b2c9f
UsmanGTA <usmangta@gmail.com>
2020-02-11 01:22:15 +0000

    prints the absolute value after linking all files together

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * _abs - computs absolute value
+ *
+ * Return: Always 0.
+ */
+
+int _abs(int n)
+{
+int mbm1 = n * -1;
+
+	if (n < 0)
+	{
+		return (mbm1);
+	}
+
+	else
+	{
+		return (n);
+	}
+}
