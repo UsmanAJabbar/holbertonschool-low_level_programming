371c09458d8632529cce27f933e39dbcd467f893
UsmanGTA <usmangta@gmail.com>
2020-02-13 16:50:15 +0000

    first commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints 0-9 once
+ * @sdnums - variable abbre based off
+ * single digit numbers for simplicity
+ */
+
+void print_numbers(void)
+{
+	int sdnums = 0;
+
+	for (sdnums = '0'; sdnums <= '9'; sdnums++)
+	{
+		_putchar(sdnums);
+		_putchar('\n');
+	}
+}
