be76cdd8555d1d9b13197269f412262e5adb5085
UsmanGTA <usmangta@gmail.com>
2020-02-13 19:32:14 +0000

    first commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints num 0-14
+ */
+
+void more_numbers(void)
+{
+	int fd;
+	int ld;
+
+	for (fd = 0; fd <= 9; fd++)
+	{
+		for (ld = 0; ld <= 14; ld++)
+		{
+			if (ld > 9)
+			{
+				_putchar((ld / 10) + '0');
+			}
+			_putchar((ld % 10) + '0');
+		}
+	_putchar('\n');
+	}
+}
