3d4dd10e224c76a6bfe3401098261c59701d21c1
UsmanGTA <usmangta@gmail.com>
2020-02-13 02:16:18 +0000

    fixed for real!

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 
 /**
- * _abs - computs absolute value
+ * times_table - computs absolute value
  *
- * @n: pulled from the main file
+ * @a: var 1
+ * @b: var 2
  *
  * Return: Returns result.
  */
@@ -14,11 +15,9 @@ void times_table(void)
 	int b;
 	int ab;
 
-	a = 0;
-	while (a <= 9)
+	for (a = 0; a < 10; a++)
 	{
-		b = 0;
-		while (b <= 9)
+		for (b = 0; b < 10; b++)
 		{
 			ab = a * b;
 
@@ -27,24 +26,22 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(ab + '0');;;
+				_putchar(ab + '0');
 			}
 
-			else if (b >= 10)
+			else if (ab >= 10)
 			{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(b / 10);
-					_putchar(b % 10);
+					_putchar((ab / 10) + '0');
+					_putchar((ab % 10) + '0');
 			}
 
 			else
 			{
-					_putchar(b + '0');
+					_putchar(ab + '0');
 			}
-		b++;
 		}
-	a++;
 	_putchar('\n');
 	}
 }
