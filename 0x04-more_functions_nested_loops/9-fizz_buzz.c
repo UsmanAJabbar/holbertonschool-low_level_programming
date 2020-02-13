8a1b219a1733832550347a535d3909b96710c362
UsmanGTA <usmangta@gmail.com>
2020-02-13 23:31:51 +0000

    first commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - contains all code
+ * @a: looping variable
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+
+		else
+		{
+			if (a % 3 == 0)
+			{
+				printf("Fizz ");
+			}
+
+			else if (a % 5 == 0 && a < 100)
+			{
+				printf("Buzz ");
+			}
+
+			else if (a == 100)
+			{
+				printf("Buzz");
+			}
+
+			else
+			{
+				printf("%d ", a);
+			}
+		}
+	}
+printf("\n");
+return (0);
+}
