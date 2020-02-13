b35ede55a0be786be73a9f5dd5c60da5bec627d6
UsmanGTA <usmangta@gmail.com>
2020-02-13 16:32:23 +0000

    first commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - checks if char is
+ * a digit, else, prints 0
+ * @c: variable imported from main.c
+ * Return: 1 if capital letter, else, 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
