39ce117599b3d4517c4a82918831017168a6d373
UsmanGTA <usmangta@gmail.com>
2020-02-13 17:07:43 +0000

    first commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - prints 0-9 once
+ * but excludes 2 and 4
+ * @sdnums - variable abbre based off
+ * single digit numbers for simplicity
+ */
+
+void print_most_numbers(void)
+{
+	int sdnums = 0;
+
+	for (sdnums = '0'; sdnums <= '9'; sdnums++)
+	{
+		if (sdnums != '2' && sdnums != '4')
+		{
+			_putchar(sdnums);
+		}
+	}
+	_putchar('\n');
+}
