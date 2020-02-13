0faa5610ae205be5183c12a9a782be8ee7700e08
UsmanGTA <usmangta@gmail.com>
2020-02-13 16:26:14 +0000

    added betty fixes

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,20 @@
 #include "holberton.h"
 
-/* comments go here */
+/**
+ * _isupper - checks if letter is
+ * capital, else, prints 0
+ * @c: variable imported from main.c
+ * Return: 1 if capital letter, else, 0
+ */
 
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 }
