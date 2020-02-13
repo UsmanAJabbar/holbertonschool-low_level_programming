df1267d53298c4fb9e76cc095d714add34e09d98
UsmanGTA <usmangta@gmail.com>
2020-02-13 16:39:43 +0000

    added desc for variables and made slight formatting changes

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,10 +2,12 @@
 
 /**
  * mul - multiplies a and b
+ * @a: first variable
+ * @b: second variable
  * Return: Returns computed value
  */
 
 int mul(int a, int b)
 {
-	return(a * b);
+	return (a * b);
 }
