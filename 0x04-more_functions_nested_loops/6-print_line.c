134bca07879a8e5870f8dc53ecd40df0453ada2d
UsmanGTA <usmangta@gmail.com>
2020-02-13 19:45:13 +0000

    first commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_line - prints line visualizing
+ * how far inputs from main are to 0
+ * @n: variable input originates from
+ * main file
+ */
+
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+_putchar('\n');
+}
