2b35f885e4d8d88d64838e70275ac9a2fc74cc65
UsmanGTA <usmangta@gmail.com>
2020-02-14 00:59:00 +0000

    first commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - prints triangle
+ * with gaps and dots
+ * @size: variable input originates from
+ * main file
+ */
+
+void print_triangle(int size)
+{
+	int rows;
+	int columns;
+	int hashes;
+
+	for (rows = 1; rows <= size; rows++)
+	{
+		for (columns = size - rows; columns > 0; columns--)
+		{
+			_putchar(' ');
+		}
+
+		for (hashes = 0; hashes < rows; hashes++)
+		{
+			_putchar('#');
+		}
+	_putchar('\n');
+	}
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
