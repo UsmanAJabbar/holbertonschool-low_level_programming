86e7dd30653e6b74376e0c59925567811dc580a8
UsmanGTA <usmangta@gmail.com>
2020-02-14 00:59:24 +0000

    added proto for triangle

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,6 +1,8 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+void print_triangle(int size);
+
 void print_most_numbers(void);
 
 int mul(int a, int b);
@@ -42,5 +44,7 @@ void print_line(int n);
 void print_diagonal(int n);
 
 void print_square(int size);
+
+void print_triangle(int size);
 
 #endif
