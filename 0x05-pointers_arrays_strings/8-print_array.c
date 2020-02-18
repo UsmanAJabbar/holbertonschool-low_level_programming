075860e948954a18f2839619b990642e76e64b1e
UsmanGTA <usmangta@gmail.com>
2020-02-18 20:01:34 +0000

    program that prints all arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints all the arrays in the loop
+ * @a: individual elements from the array stored
+ * in the pointer
+ * @n: number of elements in *a
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
