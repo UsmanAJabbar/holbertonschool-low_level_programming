00e2b9482afbc25c6052839e87c60af9acddb5d3
UsmanGTA <usmangta@gmail.com>
2020-02-20 16:14:00 +0000

    added a function that cats 2 strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,31 @@
 #include "holberton.h"
 
-c    har *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
-	for (i = 0; i <= ;??.naduewfjwebfwbfhjewbf i++
+	int destlen = _strlen(dest);
+	int i;
+
+	for (i = 0; i <= destlen; i++)
+	{
+		dest[destlen + i] = src [i];
+	}
+	dest[destlen + i] = '\0';
+
+return (dest);
+}
+
+/**
+ * _strlen - reverses the string
+ * @s: imported
+ * Return: length to print_rev
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+		continue;
+
+	return (len);
 }
