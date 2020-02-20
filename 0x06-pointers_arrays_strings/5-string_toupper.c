bb33c27cb507335a7de4e6ae9c21e438566033f3
UsmanGTA <usmangta@gmail.com>
2020-02-20 21:29:40 +0000

    first commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+char *string_toupper(char *a)
+{
+	int i;
+
+	for(i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+		{
+			a[i] -= 32;
+		}
+	}
+	return (a);
+}
+
+
+
+
+#include "holberton.h"
+
+/**
+ * _strlen - function prints length
+ * @s: pointer imported from c
+ * Return: (0)
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+		continue;
+
+	return (len);
+}
