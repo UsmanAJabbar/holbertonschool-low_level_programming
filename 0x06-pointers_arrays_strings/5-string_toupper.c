8d01304238e95ba098df640bc8cd3be8ae9b7c20
UsmanGTA <usmangta@gmail.com>
2020-02-21 03:47:24 +0000

    first commit

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,10 +1,18 @@
 #include "holberton.h"
 
+/**
+ * string_toupper - changes all
+ * letters to uppercase
+ * @a: array from main
+ * Return: Returns final value of a
+ * for use in main.
+ */
+
 char *string_toupper(char *a)
 {
 	int i;
 
-	for(i = 0; a[i] != '\0'; i++)
+	for (i = 0; a[i] != '\0'; i++)
 	{
 		if (a[i] >= 'a' && a[i] <= 'z')
 		{
