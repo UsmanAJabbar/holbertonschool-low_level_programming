226d833f401da8aae25a3eff0ffabd9d8acefc4f
UsmanGTA <usmangta@gmail.com>
2020-02-24 21:26:49 +0000

    first commit

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+				if (accept[j] == s[i])
+					return(s + i);
+		}
+	}
+return(0);
+}
