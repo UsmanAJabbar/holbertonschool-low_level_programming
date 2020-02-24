5abee13db392c1bdd0cf17177760004a3bfe8a02
UsmanGTA <usmangta@gmail.com>
2020-02-24 15:15:02 +0000

    first commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _memset - takes value from pointer s
+ * replaces with values from b
+ * @b: var that needs to be replaced
+ * @n: number of bytes/characters to be reassigned
+ * @s: pointer contains value stored in s.
+ * Return: Returns new value to pointer s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n > 0)
+	{
+		*s = b;
+		s++;
+		n--;
+	}
+
+return (s);
+}
