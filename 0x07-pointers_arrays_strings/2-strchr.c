8bcace02a3596993e764051ed8a12625b180a8fa
UsmanGTA <usmangta@gmail.com>
2020-02-24 16:35:23 +0000

    first commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strchr - prints out everything after
+ * it finds the character stored in c
+ * @c: contains character to search for.
+ * @s: string imported from main
+ * Return: Returns new value to pointer s.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s != '\0'; s++)
+	{
+		if (s[index] == c)
+			return (s);
+	}
+
+return ('\0');
+}
