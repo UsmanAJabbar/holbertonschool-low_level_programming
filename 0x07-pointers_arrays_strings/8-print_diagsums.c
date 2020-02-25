3512102755bb9c1d5e51c87fb26ab0b021fabf18
UsmanGTA <usmangta@gmail.com>
2020-02-25 05:00:16 +0000

    initial commit

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_diagsums - prints square arrays
+ * diagonally on both sides
+ * @a: array imported from a
+ * @size: size of integers
+ * 1st Matrix
+ * add 0[0] index to 1[1] to the 2[2] index
+ * 2nd matrix ( Reverse )
+ * then 0[2] to 1[1] to 2[0]
+ */
+
+void print_diagsums(int *a, int size);
