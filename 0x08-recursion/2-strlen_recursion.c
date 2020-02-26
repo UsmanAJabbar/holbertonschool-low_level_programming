dac46dfded6338113df5a83f65584c54f6ffd7d8
UsmanGTA <usmangta@gmail.com>
2020-02-26 20:14:25 +0000

    first commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - counts all indexes
+ * ... (1 + _) FOR EACH ITERATATION OF THE FUNCTION
+ * IT COUNTS ONE
+ * @s: contains string
+ * the first character printed
+ * Returns: Results to fill in values
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	return (1 + _strlen_recursion(s + 1));
+}
