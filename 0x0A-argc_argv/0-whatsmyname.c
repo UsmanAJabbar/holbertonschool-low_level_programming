ff847cb26558d7b5ecd16cfc51f9ac6d09fdd653
UsmanGTA <usmangta@gmail.com>
2020-02-29 05:31:46 +0000

    first commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,14 +3,13 @@
 
 /**
  * main - main function
- * @argc - argument count
- * @argv - argument verbose
+ * @argc: argument count
+ * @argv: argument verbose
  * (aka words saved in arrays)
- * @i = used to run through indexes
  * Return: Always 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	int i;
 
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - contains logic for function
+ * @argc: argument count
+ * @argv: disabled
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+return (0);
+}
