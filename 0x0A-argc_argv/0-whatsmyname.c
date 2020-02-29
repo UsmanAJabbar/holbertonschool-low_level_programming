b8258d419450b0c362f07c83d715d10bd41405ba
UsmanGTA <usmangta@gmail.com>
2020-02-29 03:46:53 +0000

    added betty changes

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - main function
+ * @argc - argument count
+ * @argv - argument verbose
+ * (aka words saved in arrays)
+ * @i = used to run through indexes
+ * Return: Always 0
+ */
+
 int main(int argc, char *argv[])
 {
 	int i;
