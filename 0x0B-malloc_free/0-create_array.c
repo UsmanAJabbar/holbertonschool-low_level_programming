3d2c6f5568fbba9c463f42efd13486556b3299ff
UsmanGTA <usmangta@gmail.com>
2020-03-03 20:24:13 +0000

    added edge case where c is null

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,10 +16,10 @@ char *create_array(unsigned int size, char c)
 
 	str = malloc(sizeof(char) * size);
 
-        if (size == 0)
-        {
-                return (NULL);
-        }
+	if (size == 0 || str == '\0')
+	{
+		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
 	{
