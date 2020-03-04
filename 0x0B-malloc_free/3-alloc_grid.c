eb95022ae22f3f5aac104246a0bbb38329a8a92f
UsmanGTA <usmangta@gmail.com>
2020-03-04 03:29:47 +0000

    added

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * alloc_grid - main function
+ * @width: width
+ * @height: height
+ * Return: pointer
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **pointer;
+	int rows;
+	int columns;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	pointer = malloc(sizeof(int *) * height);
+
+	if (pointer == NULL)
+	{
+		return (NULL);
+	}
+
+	for (rows = 0; rows < height; rows++)
+	{
+		pointer[rows] = malloc(sizeof(int *) * width);
+
+		if (pointer[rows] == NULL)
+		{
+			return (NULL);
+		}
+
+		for (columns = 0; columns < width; columns++)
+		{
+			pointer[rows][columns] = 0;
+		}
+	}
+	return (pointer);
+}
