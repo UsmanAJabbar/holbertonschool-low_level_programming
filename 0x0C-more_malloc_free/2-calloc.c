23fb83e36986cf99c030b8eec873f7fe2d6ea3ed
UsmanGTA <usmangta@gmail.com>
2020-03-05 21:07:04 +0000

    added

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _calloc - contains logic to
+ * create your own calloc
+ * @nmemb: number of members in
+ * array
+ * @size: malloc (sizeof) from main
+ * Return: NULL | function
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *function;
+	unsigned int index;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	function = malloc(sizeof(int) * nmemb);
+
+	if (function == NULL)
+	{
+		return (NULL);
+	}
+
+	for (index = 0; index < size; index++)
+	{
+		function[index] = 0;
+	}
+
+	return (function);
+}
