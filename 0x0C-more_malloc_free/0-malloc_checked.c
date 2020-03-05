c71fa461add881db3522160f1338e4c88c0537ed
UsmanGTA <usmangta@gmail.com>
2020-03-05 18:31:40 +0000

    added

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * malloc_checked - checks if malloc
+ * is returning null
+ * @b: assigned unsigned int from main
+ * Return: if malloc fails, returns 98
+ * status code
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *checker;
+
+	checker = malloc(b);
+
+	if (checker == NULL)
+	{
+		return (98);
+	}
+}
