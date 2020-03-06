aae915edf3362aa530118f1aac6151200b6c238c
UsmanGTA <usmangta@gmail.com>
2020-03-06 20:25:31 +0000

    added @ PLD

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * main - contains all logic
+ * @argc: argument counts
+ * @argv: argument verbose
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int result = 1;
+	int *return;
+
+	if (argc == 3)
+	{
+		return = malloc(sizeof(int) * result);
+
+		for (i = 1; i < argc; i++)
+		{
+			result = result * atoi(argv[i]);
+		}
+
+	printf("%d\n", result);
+	return (0);
+	}
+
+	else
+	{
+		printf("Error\n");
+		exit (98);
+	}
+}
