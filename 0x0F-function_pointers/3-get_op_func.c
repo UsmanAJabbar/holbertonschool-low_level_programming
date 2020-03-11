864510247ecdf59a1e60087bdd44c40845ab3d9c
UsmanGTA <usmangta@gmail.com>
2020-03-11 22:05:08 +0000

    added broken

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -13,4 +13,14 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}
 	};
 
-	int i;
+	int i = 0;
+
+	while (i < 6)
+	{
+		if strcmp(ops[i].op, s)
+			return (ops[i].f);
+		else
+			i++;
+	}
+return(NULL);
+}
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+int main(int argc, char *argv[])
+{
+
+	int a = atoi(argv[1]);
+	int (*calc)(int, int) = (*get_op_func(argv[2]));
+	int b = atoi(argv[3]);
+
+	if (argc != 4)
+	{
+		printf("Error");
+		exit (98);
+	}
+
+	if (get_op_func(argv[2] == NULL))
+	{
+		printf("Error");
+		exit (99);
+	}
+
+	if ((atoi(argv[3]) == 0) && (argv[2] == '/' || argv[2] == '%'))
+	{
+		printf("Error");
+		exit (100);
+	}
+
+	printf("%d", calc(a, b));
+
+return (0);
+}
