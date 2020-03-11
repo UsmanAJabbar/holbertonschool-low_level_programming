e0e94fb1cfbef07b4573bf6d423b7e90a2613ee9
UsmanGTA <usmangta@gmail.com>
2020-03-11 17:26:18 +0000

    fixed

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,7 +16,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int index;
 
-	if ((array == NULL) && (action == NULL) && (size => 0))
+	if ((array != NULL) && (action != NULL))
 	{
 		for (index = 0; index < size; index++)
 		{
