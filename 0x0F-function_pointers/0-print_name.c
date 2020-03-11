5ec27b58b491d18f48f0ac6ffcd2d13cfd510130
UsmanGTA <usmangta@gmail.com>
2020-03-11 12:59:31 +0000

    first commits

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints strings from main
+ * @name: pointer to names
+ * @f: calls on the functions from main
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	void (*f)(name);
+}
