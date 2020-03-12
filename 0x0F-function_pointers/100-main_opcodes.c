bd3673e001c5b7c0873ca27477c97a1b49cac6ea
UsmanGTA <usmangta@gmail.com>
2020-03-12 02:29:55 +0000

    added skeleton

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - contains logic
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	return (0);
+}
