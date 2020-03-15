cca62c20ec3049ae3cda57669ae5d0c35cf0b341
UsmanGTA <usmangta@gmail.com>
2020-03-15 00:17:53 +0000

    repositioned stdarg header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,7 @@
-#include <stdarg.h>
-
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
