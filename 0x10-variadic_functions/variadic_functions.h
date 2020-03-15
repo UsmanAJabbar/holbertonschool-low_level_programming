36c4e8df18b43640b8d79cc7ce9dacff4e9d391c
UsmanGTA <usmangta@gmail.com>
2020-03-15 19:16:30 +0000

    added 3 functions back

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,21 @@
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/* Number 3 */
+typedef struct options {
+        char s;
+        void (*f)(va_list);
+        } options;
+
+/** Declarations **/
+void printf_c(va_list);
+void printf_i(va_list);
+void printf_f(va_list);
+void printf_str(va_list);
+
 #endif
