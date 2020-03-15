736eea990b00ccf3e1ddbfce73e1f072a5f0f176
UsmanGTA <46794411+UsmanGTA@users.noreply.github.com>
2020-03-15 13:33:48 -0500

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,17 +1,16 @@
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
-
 #include <stdarg.h>
-
-typedef struct options {
-	char s;
-	void (*f)(va_list);
-	} options;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+typedef struct options {
+	char s;
+	void (*f)(va_list);
+	} options;
 
 void printf_c(va_list);
 void printf_i(va_list);
