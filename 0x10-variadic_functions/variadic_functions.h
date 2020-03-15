3604b058a4de6ee7a67b9d5f3fff80eaa6172e36
UsmanGTA <usmangta@gmail.com>
2020-03-15 19:35:07 +0000

    added

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,6 +6,18 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct options - function picker
+ * @s: imports character
+ * @f: chooses the appropriate function
+ * options: typedef
+ */
+typedef struct options
+{
+	char s;
+	void (*f)(va_list);
+} options;
+
 /** Declarations For Task 3 **/
 void printf_c(va_list);
 void printf_i(va_list);
