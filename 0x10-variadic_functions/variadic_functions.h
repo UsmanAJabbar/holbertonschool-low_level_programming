bffc11e521d6d46dc70c59b59707fe2370ae8a29
UsmanGTA <usmangta@gmail.com>
2020-03-15 19:05:59 +0000

    added

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,8 +9,4 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void printf_c(va_list);
-void printf_i(va_list);
-void printf_f(va_list);
-void printf_str(va_list);
 #endif
