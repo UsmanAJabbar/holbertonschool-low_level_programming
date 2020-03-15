099cbb3d121750a176750eed3c11db9697e4f53e
UsmanGTA <usmangta@gmail.com>
2020-03-15 19:26:25 +0000

    removing the typedef permnanently

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,13 +6,7 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-/* Number 3 */
-struct options {
-        char s;
-        void (*f)(va_list);
-        } options;
-
-/** Declarations **/
+/** Declarations For Task 3 **/
 void printf_c(va_list);
 void printf_i(va_list);
 void printf_f(va_list);
