274994447ed9c0643735ad7c733a7c0726879ad4
UsmanGTA <46794411+UsmanGTA@users.noreply.github.com>
2020-03-15 13:40:04 -0500

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,6 +15,11 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(input, n);
 
+	if (n == 0)
+	{
+		return (0);
+	}
+
 	for (index = 0; index < n; index++)
 	{
 		sum += va_arg(input, int);
