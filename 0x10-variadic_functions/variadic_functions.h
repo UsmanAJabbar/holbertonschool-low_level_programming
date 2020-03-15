e0fc4fdd56e5d444c22d48eb2c123cca8c186959
UsmanGTA <usmangta@gmail.com>
2020-03-15 19:09:05 +0000

    added

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,5 @@
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
-#include <stdarg.h>
-typedef struct options {
-	char s;
-	void (*f)(va_list);
-	} options;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
