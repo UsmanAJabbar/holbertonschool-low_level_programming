524baf699d4cfdfa4e9d87f7a5c9ddfa01768a32
UsmanGTA <usmangta@gmail.com>
2020-03-20 04:00:54 +0000

    added code for 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_list - frees all
+ * @head: contains the template
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
