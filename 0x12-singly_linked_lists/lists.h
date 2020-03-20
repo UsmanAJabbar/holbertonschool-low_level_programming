428d820430b0568519a53c519447b45c2baa9d84
UsmanGTA <usmangta@gmail.com>
2020-03-20 02:11:59 +0000

    added guarded headers

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#define _LISTS_H
+#ifndef _LISTS_H
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -18,3 +21,5 @@ size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 int _strlen(const char *s);
+
+#endif
