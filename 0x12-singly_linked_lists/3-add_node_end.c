22f64f50bb053df4b28e0a6c2d605300d29eeb94
UsmanGTA <usmangta@gmail.com>
2020-03-20 03:40:18 +0000

    added code for 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+
+	/* allocating space for the new node */
+	list_t *new = malloc(sizeof(list_t));
+	list_t *last;
+	char *string = strdup(str);
+
+	/* malloc check */
+	if (new == NULL)
+	{
+		free(string);
+		return (NULL);
+	}
+
+	new->str = string;
+	new->len = _strlen(str);
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return(new);
+	}
+
+	last = *head;
+	while (last->next != NULL)
+	{
+		last = last->next;
+	}
+
+	last->next = new;
+
+	return (new);
+}
+
+/**
+ * _strlen - function prints length
+ * @s: pointer imported from c
+ * Return: (0)
+ */
+
+int _strlen(const char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+		continue;
+
+	return (len);
+}
