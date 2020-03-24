19406956a428eced243e5ffff38f674762e57db2
UsmanGTA <usmangta@gmail.com>
2020-03-24 04:05:23 +0000

    added code for number 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - adds node
+ * to anywhere in the index
+ * @head: imports address of head
+ * @idx: index where node should be added
+ * @n: int to be added in the new node
+ * Return: Returns the address of the new node
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i; /* used for the for loop */
+	listint_t *temp = *head; /* copies *head contents to temp */
+	listint_t *newnode = malloc(sizeof(listint_t)); /*newnode*/
+
+	/* newnode - malloc check */
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+
+	/* get to the nth index referred by idx */
+	for (i = 0; i < idx; i++)
+	{
+		temp = temp->next;
+	}
+
+	/* now that you're there, create a new node */
+	/* copy what temp->next was pointing to */
+	/* now newnode should point to the same node */
+	/* temp->next was pointing to */
+	newnode->n = n; /* copy given data into it */
+	newnode->next = temp->next; /* copy the address of temp->next */
+
+	/* get temp->next to point to our new node */
+	temp->next = newnode;
+
+return (newnode);
+}
