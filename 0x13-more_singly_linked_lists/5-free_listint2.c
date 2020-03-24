2deab30b6bc1c9cdf3e4118bd23a48f973785c35
UsmanGTA <usmangta@gmail.com>
2020-03-24 06:41:54 +0000

    added edge case where heads NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,6 +13,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp; /* temp node to avoid changing head */
 
+	if (head == NULL || *head == NULL)
+	{
+		return;
+	}
+
 	while (*head != NULL)
 	{
 		temp = *head; /* temp now has address to the next node */
