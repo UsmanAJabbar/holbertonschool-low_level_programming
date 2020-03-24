d7c1091a5b7475b86823a070d9b4260b46fc10ae
UsmanGTA <usmangta@gmail.com>
2020-03-24 00:27:11 +0000

    added code for 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees all
+ * nodes pointed to a pointer to
+ * the head.
+ * @head: pointer pointing to the
+ * address of head
+ * Return: Void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp; /* temp node to avoid changing head */
+
+	while (*head != NULL)
+	{
+		temp = *head; /* temp now has address to the next node */
+		*head = temp->next; /* update head to increment */
+		free(temp); /* free the block */
+	}
+	*head = NULL; /* set head to null */
+}
