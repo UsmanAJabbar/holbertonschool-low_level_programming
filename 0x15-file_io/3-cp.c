9913dff0d23c988ba768a75f60863ad6c08069db
UsmanGTA <usmangta@gmail.com>
2020-03-31 23:36:56 +0000

    added compressed code to comply with betty reqs

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,6 +4,13 @@
 #define SRC argv[1]
 #define DEST argv[2]
 
+/**
+ * main - replicates cp function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
 	int src, dest, readcount, write_d;
@@ -14,56 +21,36 @@ int main(int argc, char *argv[])
 		printf("Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	else
+	/* Open the src & dest + Check Success */
+	src = open(SRC, O_RDWR);
+	/* Copy everything from source into the 1024 bit buffer */
+	readcount = read(src, buffer, 1024);
+	if (src == -1 || readcount == -1)
 	{
-		/* Open the src & dest + Check Success */
-		src = open(SRC, O_RDWR);
-		if (src == -1)
-		{
-			printf("Error: Can't read from file NAME_OF_THE_FILE\n");
-			exit(98);
-		}
-
-		dest = open(DEST, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-		if (dest == -1)
-		{
-			printf("Error: Can't write to NAME_OF_THE_FILE\n");
-			exit(99);
-		}
-
-		/* Copy everything from source into the 1024 bit buffer */
-		readcount = read(src, buffer, 1024);
-		if (readcount == -1)
-		{
-			printf("Error: Can't read from the file NAME_OF_THE_FILE\n");
-			exit(99);
-		}
-
-		/* Now write everything saved in buffer into dest as many bytes READ picked up on */
-		write_d = write(dest, buffer, readcount);
-		if (write_d == -1)
-		{
-			printf("Error: Can't write to NAME_OF_THE_FILE\n");
-			exit(99);
-		}
-
-		/* Check if closing both files is possible */
-		if (close(src) == -1)
-		{
-			printf("Error: Can't close fd FD_VALUE");
-			exit(100);
-		}
-
-		if (close(dest) == -1)
-		{
-			printf("Error: Can't close fd FD_VALUE");
-			exit(100);
-		}
-
-		/* Closing them both is possible, clear memory */
-		close(src);
-		close(dest);
+		printf("Error: Can't read from file %s\n", SRC);
+		exit(98);
 	}
-	return (0);
+
+	/* Create the destination file if it doesn't exist with 664 perms */
+	dest = open(DEST, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	/* Write everything saved in buffer into dest as many bytes READCOUNT */
+	write_d = write(dest, buffer, readcount);
+	if (dest == -1 || write == -1)
+	{
+		printf("Error: Can't write to %s\n", DEST);
+		exit(99);
+	}
+	if (close(src) == -1)
+	{
+		printf("Error: Can't close fd %d\n", SRC);
+		exit(100);
+	}
+	if (close(dest) == -1)
+	{
+		printf("Error: Can't close fd %d\n", DEST);
+		exit(100);
+	}
+	close(src);
+	close(dest);
+return (0);
 }
