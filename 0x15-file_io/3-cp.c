6ff45bb6843878e1d97894551654cea54733c800
UsmanGTA <usmangta@gmail.com>
2020-04-01 03:01:37 +0000

    added edge case

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,28 +23,21 @@ int main(int argc, char *argv[])
 	}
 	/* READ OPERATIONS */
 	src = open(SRC, O_RDONLY);
-	if (src == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", SRC), exit(98);
 	/* Copy everything from source into the 1024 bit buffer */
-	readcount = read(src, buffer, 1024);
-	if (readcount == -1)
+	if (src == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", SRC), exit(98);
 	/* WRITE OPERATIONS */
 	dest = open(DEST, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	/* Write everything saved in buffer into dest as many bytes READCOUNT */
-	write_d = write(dest, buffer, readcount);
+	while ((readcount = read(src, buffer, 1024)) > 0)
+		write_d = write(dest, buffer, readcount);
+	if (readcount == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", SRC), exit(98);
 	if (dest == -1 || write_d == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", DEST), exit(99);
-	}
 	if (close(src) == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src), exit(100);
-	}
 	if (close(dest) == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest), exit(100);
-	}
-	close(src), close(dest);
 return (0);
 }
