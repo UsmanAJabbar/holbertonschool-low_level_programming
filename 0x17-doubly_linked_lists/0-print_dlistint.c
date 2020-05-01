9f3ca58b5b49c81ebccac079d7740a47588a3c59
UsmanGTA <usmangta@gmail.com>
2020-05-01 06:07:00 +0000

    added initial code for task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all of the integers in
+ * the linked list
+ * @h: Linked list
+ * Return: Returns the number of index found
+ * and printed
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int index;
+
+	/* Check if h exists */
+	if(h == NULL)
+		return(0);
+
+	/* Print all of the elements */
+	for(; h != NULL; index++, h = h->next)
+		printf("%d\n", h->n);
+
+	/* Return the number of lists printed */
+	return (index);
+}
