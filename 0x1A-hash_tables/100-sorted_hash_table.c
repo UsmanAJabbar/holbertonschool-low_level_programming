f374b46aecd77f147d25944af3444383a031fc47
UsmanGTA <usmangta@gmail.com>
2020-05-30 01:35:53 +0000

    added new code

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -7,7 +7,7 @@
  */
 shash_table_t *shash_table_create(unsigned long int size)
 {
-	shash_table_t *new_table = malloc(sizeof(hash_table_t));
+	shash_table_t *new_table = malloc(sizeof(shash_table_t));
 	unsigned long int index;
 
 	if (new_table == NULL)
@@ -17,7 +17,7 @@ shash_table_t *shash_table_create(unsigned long int size)
 	}
 
 	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	new_table->array = malloc(sizeof(shash_node_t *) * size);
 
 	if (new_table->array == NULL)
 	{
@@ -53,7 +53,7 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 		return (0);
 	new->value = strdup(value);
 	new->key = strdup(key);
-	seeker(ht, array[index], index); /* Handles sPrev and sNext */
+	seeker(ht, ht->array[index]); /* Handles sPrev and sNext */
 
 	/* CASE 1: If a singly linked list isn't there */
 	if (ht->array[index] == NULL)
@@ -72,25 +72,47 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 	return (1);
 }
 
-void *seeker(shash_table_t *ht, shash_node_t *node, unsigned long int arrayindex)
+void *seeker(shash_table_t *ht, shash_node_t *node)
 {
-	unsigned long int index = keyindex((const char *)node->key, ht)
+	unsigned long int index;
+	int i;
+	shash_node_t *search;
+	char *alpha[] = 
+			{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", 
+				"o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", NULL};
 
-	for (search = ht->array[index]; search; search = search->next)
-		if (strcmp(search->key, key) == 0)
+	for (i = 0; alpha[i] != NULL; i++)
+	{
+		index = key_index((unsigned char *)alpha, ht->size);
+		for (search = ht->array[index]; search; search = search->next)
 		{
-			if (arrayindex > index)
-				node->snext = search;
+			if (alpha[i][0] < search->key[0])
+				node->sprev = search, node->snext = NULL;
 			else
-				node->sprev = search;
+				node->snext = search, node->sprev = NULL;
 		}
+	}
+/*	1) Seeker should be able to set the sprev and snext
+	2) Seeker should go through the hash table, and look for
+	   any character, and determine if that character appears
+	   before "d" or after "d".
+	3) If a character is found and is before d, the node/s node->sprev
+	   should be set to that.
+	4) Else, if character is found and is after d, thne node/s node->snext
+       should be set to that. */
 }
 
-void sprint(shash_table_t *ht)
+/**
+ * shash_table_print - prints out all
+ * of the elements in the hash table
+ * @ht: hash table
+ * Return: Always Void
+ */
+void shash_table_print(const shash_table_t *ht)
 {
 	unsigned long int index;
 	int flag = 0;
-	hash_node_t *temp;
+	shash_node_t *temp;
 
 	if (ht)
 	{
@@ -112,17 +134,41 @@ void sprint(shash_table_t *ht)
 	}
 }
 
+void shash_table_print_rev(const shash_table_t *ht)
+{
+	unsigned long int index;
+	int flag = 0;
+	shash_node_t *temp;
+
+	if (ht)
+	{
+		printf("{");
+		for (index = 0; index < ht->size; index++)
+		{
+			if (ht->array[index])
+				/* Get to the end of the list */
+				for (temp = ht->array[index]; temp; temp = temp->next)
+						;
+				for (; temp; temp = temp->sprev)
+				{
+					if (flag == 1)
+						printf(", ");
+					printf("'%s': '%s'", temp->key, temp->value), flag = 1;
+				}
+		}
+		printf("}\n");
+	}
+}
+
 int main(void)
 {
-	shash *ht = shash_table_create(1024);
+	shash_table_t *ht = shash_table_create(1024);
 	shash_table_set(ht, "y", "0");
 	shash_table_print(ht);
 	shash_table_set(ht, "j", "1");
-	shash_table_print(ht);
+	shash_table_set(ht, "b", "3");
 	shash_table_set(ht, "c", "2");
 	shash_table_print(ht);
-	shash_table_set(ht, "b", "3");
-	shash_table_print(ht);
-	sprint(ht);
+	shash_table_print_rev(ht);
 	return (0);
 }
