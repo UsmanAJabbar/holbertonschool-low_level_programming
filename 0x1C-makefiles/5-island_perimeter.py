b762d2ef1c52beb02b8408d3c0dc47358a00a524
usmangta <usmangta@gmail.com>
2020-07-08 03:12:39 +0000

    added initial commit for task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""FAAKING DOCSTRING"""
+
+
+def island_perimeter(grid):
+    """
+    ------------------------
+    METHOD: ISLAND PERIMETER
+    ------------------------
+    DESCRIPTION:
+        Finds the perimeter of a supposed island.
+    NOTES:
+        1 = Land | 0 = Water
+    ARGS:
+        grid: list of lists containing the grid
+    """
+
+    if len(grid) < 1 or len(grid[0]) < 1:
+        return
+
+    grid_height = len(grid)
+    grid_len = len(grid[0])
+    perimeter = 0
+
+    for height in range(grid_height):          # y = vertical
+        for length in range(grid_len):         # x = horizontal
+            if grid[height][length] == 1:
+                try:
+                    # CHECK UP ^
+                    if grid[height - 1][length] == 0:
+                        perimeter += 1
+                    # CHECK DOWN v
+                    if grid[height + 1][length] == 0:
+                        perimeter += 1
+                    # CHECK LEFT <
+                    if grid[height][length - 1] == 0:
+                        perimeter += 1
+                    # CHECK RIGHT >
+                    if grid[height][length + 1] == 0:
+                        perimeter += 1
+                except IndexError:
+                    continue
+    return perimeter
