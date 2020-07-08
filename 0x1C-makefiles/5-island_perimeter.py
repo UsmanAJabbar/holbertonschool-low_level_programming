0355da7ecd76ca73e5adba3f40d560040d9910f2
usmangta <usmangta@gmail.com>
2020-07-08 03:23:01 +0000

    added allegedly new code

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -29,15 +29,28 @@ def island_perimeter(grid):
                     # CHECK UP ^
                     if grid[height - 1][length] == 0:
                         perimeter += 1
+                except IndexError:
+                    pass
+
+                try:
                     # CHECK DOWN v
                     if grid[height + 1][length] == 0:
                         perimeter += 1
+                except IndexError:
+                    pass
+
+                try:
                     # CHECK LEFT <
                     if grid[height][length - 1] == 0:
                         perimeter += 1
+                except IndexError:
+                    pass
+
+                try:
                     # CHECK RIGHT >
                     if grid[height][length + 1] == 0:
                         perimeter += 1
                 except IndexError:
-                    continue
+                    pass
+
     return perimeter
